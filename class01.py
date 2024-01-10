47bfb71c569ecf942139da36934ccd923df75e5c
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2024-01-10 20:52:06 +0530

    Add files via upload

---
+++ class01.py
@@ -0,0 +1,48 @@
+# gennerator expression
+
+# tp = [i for i in range(10)]
+# print(tp)
+
+# or
+
+# tp = (i for i in range(10))
+# print(tp)
+
+# or
+# ka = [2,3,4,5]
+# tp = (float(i) for i in ka)
+# print(tp)
+
+
+# or
+
+# ka = [2,3,4,5]
+# tp = tuple(float(i) for i in ka)
+# print(tp)
+
+# or
+
+# lst= [1,2,3,3,4]
+# new_lst=[3,54,65,lst]
+# print(new_lst)
+
+# or
+
+# u = *range(1,4),
+# print(u, type(u))
+
+# or
+
+# *st,u = *range(1,100),
+# print(type(u))
+# print(u)
+
+# or
+
+# tp = (23,3,5,446,57,[2,4],())
+# tp[-2].clear()
+# print(tp)
+
+ 
+# question:
+# lst = [(manish)]
