16947d1e51ec36178d2d49faf563439f900f5950
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2024-01-18 20:38:04 +0530

    ..

---
+++ scope of variable.py
@@ -0,0 +1,40 @@
+# def add ():
+#     #local scope
+#     a = 10
+#     print(a)
+
+# #global scope
+# a = 23
+# add()
+# print(a)
+
+
+
+# def add():
+#     global ab
+#     ab = 9
+
+# ab = 20
+# add()
+# print(ab)
+
+
+
+
+# def add():
+#     def fun1():
+#         ab = 0
+#     def fun2():
+#         ab = 100
+#     def fun3():
+#         ab = 200 
+#     ab = 33
+#     fun1()
+#     print(ab)
+#     fun2()
+#     print(ab)
+#     fun3()
+#     print(ab)
+# ab = 20
+# add()
+# print(ab)
