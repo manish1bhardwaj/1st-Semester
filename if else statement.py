f1ad8cf8a92d390781bbdd43ebe074a8890003c5
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2024-01-18 20:31:58 +0530

    ..

---
+++ if else statement.py
@@ -0,0 +1,41 @@
+# if 5==3:
+#     print("Yes")
+# else:
+#     print("NO")
+
+
+# if 5>3:
+#     print("Yes")
+# else:
+#     print("NO")
+
+# if 5>=3:
+#     print("Yes")
+# else:
+#     print("NO")
+
+
+# if 5<3:
+#     print("Yes")
+# else:
+#     print("NO")
+
+
+# if 5<=3:
+#     print("Yes")
+# else:
+#     print("NO")
+
+# # if we want to write check the multiple condition so we can use elif statement:
+
+
+if 5<3:
+    print("Yes")
+elif 4!=4:
+    print("yes")
+elif 4==8:
+    print("yes")
+else:
+    print("no")
+
+
