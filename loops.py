469ddedc847d93b83ec09d6e2941288a0d77c7fd
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2024-01-18 20:32:14 +0530

    ..

---
+++ loops.py
@@ -0,0 +1,44 @@
+# there are two types of loops in python :
+
+# for loop 
+# while loop
+
+
+# for num in range (1, 100):
+#     print("num")
+# name = "ManishBhardwaj"
+# for i in name:
+#     print(i)
+
+# colors = ["red","green"]
+# for i in colors:
+#     print(i)
+#     for j in i:
+#         print(j)
+
+# for i in range(10):
+#     print(i)
+# but i want 10 also then
+
+# for i in range(10):
+#     print(i+1)
+
+# for i in range(1,11):
+#     print(i*3)
+
+# for i in range(1,10,3): (start,stop,step)
+    #print(i)
+
+# while loop 
+
+# x=1
+# while(x<=10):
+#     x=x+1
+#     if x==4 or x==8:
+#       continue
+#     print(x)
+    
+    
+    
+for i in range(1,10,2):
+ print(i)
