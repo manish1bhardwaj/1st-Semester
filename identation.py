d115f5e3397d2d6e7ddfc189e7ba17af967865e2
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2024-01-18 20:31:07 +0530

    ..

---
+++ identation.py
@@ -0,0 +1,9 @@
+# print("Manish")
+#    print("Manish")
+
+# but
+print("manish")
+print("bhardwaj")
+
+# identation means to (tabs and spaces) that are used at the beginning of a statement.
+# the statement with the same same identation belong to the same group called a suite  
