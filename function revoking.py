613217f12daf2738dcfa9ffb0829475101d778c7
Manish bhardwaj <142594633+manish1bhardwaj@users.noreply.github.com>
2024-01-18 20:34:57 +0530

    ..

---
+++ function revoking.py
@@ -0,0 +1,10 @@
+def display_info(name,section,rollno,college):
+    return f'''
+Name : {name}
+Section : {section}
+Roll no : {rollno}
+College : {college}
+'''
+
+data = display_info('Manish','CB',25,'GLA')
+print(data)
